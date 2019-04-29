ec5c7290c153fcd3916445d68bbc2ae4a4648081
Kentaro Arai <k.arai0820@gmail.com>
2019-04-30 02:25:33 +0900

    make todolist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDoItem from './components/ToDoItem';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      todos:[
+      {id:1,text:'abc',isDone:false},
+      {id:2,text:'def',isDone:false}
+      ],
+      newTodo:{text:''}
+    };
+  };
+
+handleInput = e =>{
+const newId = Math.max.apply(null,this.state.todos.map(t=>t.id))+1;
+this.setState({
+newTodo:{id:newId,text:e.target.value isDone:false}
+
+});
+};
+createNewToDoItem = () =>{
+  this.setState(({todos,newTodo })=>({
+    todos:[
+    ... todos,newTodo],
+newTodo:{text:''}
+}));
+
+};
+deleteTodo = item =>{
+this.setState(({todos})=>({
+todos:todos.filter((todo)=>todo.id!==item.id)
+}));
+};
+
+toggleCheckBox = item =>{
+const tempTodos = this.state.todos.map(todo=>{
+  if(todo ===item){
+    todo.isDone=!todo.isDone;
+  }
+  return todo
+});
+
+this.setState(({todos}) => ({
+  todos:tempTodos
+}));
+
+};
+
   render() {
+    let remaining= this.state.todos.filter((todo)=>{
+      return !todo.isDone
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>My todo
+      <span>({this.remaining}/{this.state.todos.length})</span>
+      </h1>
+      <ul>
+      {this.state.todos.map((item,key)=>{
+        return(
+        <ToDoItem
+        key={key}
+        text={item.text}
+        toggleCheckBox={this.toggleCheckBox.bind(this,item)}
+        deleteTodo={this.deleteTodo.bind(this,item)}
+        />
+       </ul>
+       <div>
+       <input type="text" value={this.state.newTodo.text} onChange={this.handleInput}/>
+       <button onClick={this.createNewToDpItem}>+</button>
+       </div>
+
+
       </div>
+
     );
   }
 }
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class ToDoItem extends Component{
+render(){
+	return(
+		<li>
+		<input type="checkbox" onChange={this.props.toggleCheckBox}/>
+		<span>{this.props.text}</span>
+		<span onClick={this.props.deleteTodo}>[x]</span>
+		</li>
+		);
+}
+
+}
+
+export default ToDoItem;
